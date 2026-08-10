5181cd5f34b7a4348d5eafb2232535e5acdd39b1
Swagat Kumar Behera <you@example.com>
2026-08-10 21:01:24 +0530

    Initial project setup

---
+++ src/main/java/com/ats/ats_platform/AtsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ats.ats_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ats/ats_platform/AtsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.ats_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
